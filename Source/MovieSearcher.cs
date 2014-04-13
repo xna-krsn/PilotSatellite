9f24c622d6070d59f34b9a3f96d5dbb8fd00936c
evgn <evgn.kzmn@gmail.com>
2014-04-13 04:16:38 +0400

    Movie data provider added

---
+++ Source/MovieSearcher.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Newtonsoft;
+using System.Net;
+using System.IO;
+
+namespace DataProvider
+{
+    public class MovieSearcher
+    {
+        public struct MovieData
+        {
+            public string number;
+            public string imdbId;
+            public string name;
+            public string year;
+        };
+
+        private static List<MovieData> GetFoundMovies(string str)
+        {
+            Regex reg = new Regex(
+                "<[\\s]*td[\\s]*class[\\s]*=[\\s]*\"number\"[\\s]*>[\\s]*([1-9]+)[\\s\\S]*?<[\\s]*/[\\s]*td[\\s]*>" +
+                "[\\s\\S]*?<[\\s]*a[\\s]*href[\\s]*=[\\s]*\"/title/([\\S]+?)/\"[\\s]*>[\\s]*([\\s\\S]+?)[\\s]*<[\\s]*/[\\s]*a[\\s]*>" +
+                "[\\s\\S]*?<[\\s]*span[\\s]*class[\\s]*=[\\s]*\"year_type\"[\\s]*>[\\s\\S]*?([0-9]+)[\\s\\S]*?<[\\s]*/[\\s]*span[\\s]*>"
+            );
+            List<MovieData> result = new List<MovieData>();
+            Match matched = reg.Match(str);
+            while (matched.Success)
+            {
+                result.Add(new MovieData
+                {
+                    number = matched.Groups[1].Value,
+                    imdbId = matched.Groups[2].Value,
+                    name = matched.Groups[3].Value,
+                    year = matched.Groups[4].Value
+                });
+                matched = matched.NextMatch();
+            }
+            return result;
+        }
+
+        private static string GetRequestString(string param)
+        {
+            return "http://www.imdb.com/search/title?count=100&genres=sci_fi&plot=" + param + "&title_type=feature,documentary";
+        }
+
+        public static List<MovieData> SearchMovies(string param)
+        {
+            HttpWebRequest request = null;
+            HttpWebResponse response = null;
+            StreamReader readStream = null;
+            try
+            {
+                request = (HttpWebRequest)WebRequest.Create(GetRequestString(param));
+                response = (HttpWebResponse)request.GetResponse();
+                readStream = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                return GetFoundMovies(readStream.ReadToEnd());
+            }
+            finally
+            {
+                readStream.Close();
+                response.Close();
+            }
+        }
+    }
+}
---
+++ Source/MoviesDataProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataProvider
+{
+    public class MoviesDataProvider
+    {
+        private Dictionary<string, List<MovieSearcher.MovieData>> tagMovies = new Dictionary<string, List<MovieSearcher.MovieData>>();
+        private Dictionary<string, List<string>> movieTags = new Dictionary<string, List<string>>();
+
+        public MoviesDataProvider(List<string> tags)
+        {
+            foreach(var tag in tags)
+            {
+                var movies = MovieSearcher.SearchMovies(tag);
+                tagMovies.Add(tag, movies);
+                foreach (var movie in movies)
+                {
+                    if (movieTags.ContainsKey(movie.name))
+                        movieTags[movie.name].Add(tag);
+                    else
+                    {
+                        var tagList = new List<string>();
+                        tagList.Add(tag);
+                        movieTags.Add(movie.name, tagList);
+                    }
+                }
+            }
+        }
+
+        public List<MovieSearcher.MovieData> GetMoviesForTag(string tag)
+        {
+            if (!tagMovies.ContainsKey(tag))
+                throw new Exception("Get movies failed: tag not found");
+            return tagMovies[tag];
+        }
+
+        public List<string> GetTagsForMovie(MovieSearcher.MovieData movie)
+        {
+            if(!movieTags.ContainsKey(movie.name))
+                throw new Exception("Get tag failed: movie not found");
+            return movieTags[movie.name];
+        }
+    }
+}
